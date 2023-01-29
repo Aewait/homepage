19638a798d39efb93f4f9eb1a11c3ab40ea8c816
Ewait <s2522446388@163.com>
2023-01-29 12:02:09 +0800

    修改 壁纸使用bing

--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,13 @@ export const mainStore = defineStore("main", {
     state: () => {
         return {
             innerWidth: null, // 当前窗口宽度
-            coverType: "0", // 壁纸种类
+            coverType: "1", // 壁纸种类
             musicIsOk: false, // 音乐是否加载完成
             musicVolume: 0, // 音乐音量;
             musicOpenState: false, // 音乐面板开启状态
             backgroundShow: false, // 壁纸展示状态
             boxOpenState: false, // 盒子开启状态
-            mobileOpenState: false, // 移动端开启状态
+            mobileOpenState: true, // 移动端开启状态
             mobileFuncState: false, // 移动端功能区开启状态
             setOpenState: false, // 设置页面开启状态
             playerState: false, // 当前播放状态
